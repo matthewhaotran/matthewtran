4286b33be606f28a4a205af284442dce73126fcd
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2017-10-16 15:57:41 -0700

    set up heroku push

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ const $ = require('gulp-load-plugins')();
 
 gulp.task('copy:fonts', () =>
   gulp
-  .src('node_modules/font-awesome/fonts/*')
+  .src(['node_modules/font-awesome/fonts/*'])
   .pipe(gulp.dest('dist/fonts'))
+);
+
+gulp.task('copy:carouselFonts', () =>
+  gulp
+  .src(['node_modules/angular-ui-carousel/dist/fonts/*'])
+  .pipe(gulp.dest('dist/css/fonts'))
 );
 
 gulp.task('copy:html', () =>
@@ -13,17 +19,27 @@ gulp.task('copy:html', () =>
   .pipe(gulp.dest('dist'))
 );
 
+gulp.task('copy:images', () =>
+  gulp
+  .src('client/app/images/*')
+  .pipe(gulp.dest('dist/images'))
+);
+
 gulp.task('build:js', () =>
   gulp
   .src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
     'node_modules/angular/angular.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.js',
+    'node_modules/angular-ui-carousel/dist/ui-carousel.min.js',
     'client/**/*.module.js',
     'client/**/*.js'
   ])
   .pipe($.concat('bundle.min.js'))
-  .pipe($.uglify().on('error', (err) => {
-    console.log(`Error: ${err}`);
-  }))
+  // .pipe($.uglify().on('error', (err) => {
+  //   console.log(`Error: ${err}`);
+  // }))
   .pipe(gulp.dest('dist/js'))
 );
 
@@ -32,6 +48,7 @@ gulp.task('build:css', () =>
   .src([
     'node_modules/bootstrap/dist/css/bootstrap.css',
     'node_modules/font-awesome/css/font-awesome.css',
+    'node_modules/angular-ui-carousel/dist/ui-carousel.min.css',
     'client/**/*.css'
   ])
   .pipe($.concat('bundle.min.css'))
@@ -54,4 +71,6 @@ gulp.task('serve', () =>
   })
 );
 
-gulp.task('default', ['copy:fonts', 'copy:html', 'build:js', 'build:css', 'watch', 'serve']);
+gulp.task('build', ['copy:fonts', 'copy:images', 'copy:html', 'build:js', 'build:css', 'copy:carouselFonts'])
+
+gulp.task('default', ['build', 'watch', 'serve']);
