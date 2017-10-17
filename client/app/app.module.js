5cc9f69f7d90011d1330b4fd91d04e6b9307855f
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2017-10-16 19:37:25 -0700

    added home and about state

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -3,10 +3,33 @@
 
     angular.module('app', [
         // Angular modules
+        
 
         // Custom modules
+        'app.home',
+        'app.about',
 
         // 3rd Party Modules
+        'ui.router'
 
-    ]);
+    ]).config(appConfig);
+
+    appConfig.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+    function appConfig($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider.state('home', {
+            url: '/home',
+            controller: 'HomeController as homeCtrl',
+            templateUrl: 'app/home/home.template.html'
+        });
+
+        $stateProvider.state('about', {
+            url: '/about',
+            controller: 'AboutController as aboutCtrl',
+            templateUrl: 'app/about/about.template.html'
+        });
+
+    };
 })();
