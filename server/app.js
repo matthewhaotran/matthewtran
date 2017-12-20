0ecd70a20364fff5b1b5d3813936ff9ecc1430d1
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2017-12-19 19:39:25 -0800

    added portfolio page

--- server/app.js
+++ server/app.js
@@ -3,12 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/../dist`));
+
 app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../dist`));
 
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/../dist/index.html`);
+});
+
+app.get('/#portfolio', (req, res) => {
+  res.sendFile(`${__dirname}/../dist/portfolio.html`);
 });
 
 module.exports = app;
